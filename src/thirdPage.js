b56423a6521b731a44a2951bc4603d0bf264dbb9
Saatvik <saatvik.jbp@gmail.com>
2024-01-04 12:52:56 +0530

    modified

--- src/thirdPage.js
+++ src/thirdPage.js
@@ -1,7 +1,9 @@
 export default function Contact(){
     const mainDiv = document.createElement('div');
     mainDiv.setAttribute('class', 'mainDiv');
-    mainDiv.innerHTML = "<h1>//Drop your third page</h1>"
+    mainDiv.innerHTML = `<div class="sub-container"><h1>Phone Number</h1>
+<p>1122338888</p> <p>0569-2345678</p>
+<h1>Address</h1> <p>345, plot-A, Aurobindo Nagar</p> <p>House A, Madras Cafe, Chennai</p></div>`
 
     return mainDiv;
 }
