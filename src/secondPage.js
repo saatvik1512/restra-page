0c4e2635ade774c8750d57fbe4fa26c11589ef32
Saatvik <saatvik.jbp@gmail.com>
2024-01-04 22:03:19 +0530

    done partially

--- src/secondPage.js
+++ src/secondPage.js
@@ -1,6 +1,12 @@
 export default function Food(){
     const mainDiv = document.createElement('div');
     mainDiv.setAttribute('class', 'mainDiv');
-    mainDiv.innerHTML = "<h1>//Drop your second page</h1>"
+    mainDiv.innerHTML = `<div class="second-container">
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div>
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div>
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div>
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div>
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div>
+    <div class="card"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg" alt="Food"></div></div>`
     return mainDiv;
 }
