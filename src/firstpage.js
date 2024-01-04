ffb92d518613757225120c117360c4e77c975406
Saatvik <saatvik.jbp@gmail.com>
2024-01-04 14:02:26 +0530

    first and third done

--- src/firstpage.js
+++ src/firstpage.js
@@ -2,7 +2,9 @@ export default function Home(){
     const mainDiv = document.createElement('div');
     mainDiv.setAttribute('class', 'mainDiv');
     mainDiv.classList.add('active')
-    mainDiv.innerHTML = "<h1>//Drop your first page</h1>"
+    mainDiv.innerHTML = `<div class='first-container'><h1>Nothing brings <span>people</span> together like <span>Good</span>food</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Gordon_Ramsay.jpg" alt="gordon ramsay">
+    </div>`
 
     return mainDiv;
 }
